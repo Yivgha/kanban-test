44baef2fe21150f926797f78c7920d5411dc0d55
Yivgha <genieukne@gmail.com>
2024-10-04 17:19:45 +0300

    Change formatting configs and workflow file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'eslint-define-config';
 import globals from 'globals';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReact from 'eslint-plugin-react';
 
 export default defineConfig([
   // Global ignores
   {
-    ignores: ['node_modules/', 'dist/', 'build/'], // Add your ignored paths here
+    ignores: ['node_modules/'],
   },
 
   // Backend-specific configuration
@@ -28,6 +29,29 @@ export default defineConfig([
     },
     rules: {
       'prettier/prettier': 'warn',
+    },
+  },
+  // Frontend-specific configuration
+  {
+    files: ['./front-end/**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: eslintPluginPrettier,
+      react: eslintPluginReact,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+      'react/react-in-jsx-scope': 'off',
     },
   },
 ]);

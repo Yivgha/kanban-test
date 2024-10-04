3db3dce58919d95277369f21cc23116c1beb8e97
Yivgha <genieukne@gmail.com>
2024-10-04 15:13:33 +0300

    Restore branch without fe

--- back-end/server.ts
+++ back-end/server.ts
@@ -31,5 +31,5 @@ async function startServer(): Promise<void> {
 startServer();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world! Test 1.1.9');
+  res.send('Hello, world! Test 1.1.10');
 });

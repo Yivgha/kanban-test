d520561f36198c25933a9fe6dba18719a7eeaacf
Yivgha <genieukne@gmail.com>
2024-10-04 18:17:51 +0300

    Fix ts packages

--- back-end/server.ts
+++ back-end/server.ts
@@ -32,5 +32,5 @@ async function startServer(): Promise<void> {
 startServer();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world! Test 1.1.12');
+  res.send('Hello, world! Test 1.1.13');
 });
